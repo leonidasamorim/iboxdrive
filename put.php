64b72c8e2451a2f9f8baf5eaf42b1499400b6846
Davy Amorim <leonidasamorim@gmail.com>
2019-09-14 13:40:05 -0300

    ajuste arquivo put

--- put.php
+++ put.php
@@ -1,20 +1,39 @@
 <?
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    $file       = $_GET['url'];
+    $file       = str_replace('https:/', 'https://', $file);
+    $file       = str_replace('http:/', 'http://', $file);
+    $hosturl       = parse_url($file);
+    $domainurl  = $hosturl["host"];
+    $domain     = $_SERVER['SERVER_NAME'];
+    $protocol   = $_SERVER['SERVER_PROTOCOL'];
 
 
-var_dump($_GET);
-exit;
 
-$file = $_GET['file'];
+    if (checkRemoteFile($file)){
 
-if ($this->checkRemoteFile($file)){
+        $folderdomain = "files/".$domainurl;
+        if (!file_exists($folderdomain)) {
+            mkdir($folderdomain);
+        }
+
+        file_put_contents($folderdomain ."/". basename($file), fopen($file, 'r'));
+
+        $url = 'http://'. $domain .'/'. $folderdomain. '/'. basename($file);
+        Header("Location: ". $url);
+        exit;
+
+    }else{
+        echo "File not found.";
+    }
 
 
-}else{
-
-}
 
 
-function checkRemoteFile($url)
+
+ function checkRemoteFile($url)
 {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,$url);
